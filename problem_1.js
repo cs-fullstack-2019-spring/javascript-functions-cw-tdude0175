e5a944bbfa13b4e7f01bdfb8f5ce8c8817daf763
tdude0175 <tdude0175@gmail.com>
2019-01-11 16:03:40 -0600

    completed graded exercises 1 and 2

---
+++ problem_1.js
@@ -0,0 +1,28 @@
+//Using the code below:
+//
+// printNumbers();
+//
+// Create a printNumbers function to print integers from -25 to 20.
+
+
+
+//you can set the parameters to be  the start in the loop or the end of the loop
+//remember differentiate less than [<] and greater than [>]
+
+function printNumbers()
+{
+    for(var start = -25; start <= 20; start++)
+    {
+        console.log(start)
+    }
+
+}
+
+
+
+//you can set the parameters to be  the start in the loop or the end of the loop
+
+
+
+
+printNumbers();
---
+++ problem_2.js
@@ -0,0 +1,13 @@
+//Create a function that’s passed a firstName and lastName.
+// Return lastName comma firstName.
+
+
+
+
+function yourName(firstName,lastName)
+{
+    console.log(firstName + " , " + lastName );
+}
+
+
+yourName("Thomas","Brown");
